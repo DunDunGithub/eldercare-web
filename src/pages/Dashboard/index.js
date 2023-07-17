cc03ee8d88d8a939abba644d345d40700198156e
Dun <dun@MYD92s-MacBook-Pro.local>
2023-07-17 17:53:24 +0700

    Test

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,8 +2,6 @@ import classNames from 'classnames/bind';
 import styles from './Dashboard.module.scss';
 import HeaderDashboard from '~/components/HeaderDashboard';
 import NavigationDashboard from '~/components/NavigationDashboard';
-import DataViewCalender from '~/components/DataViewCalender';
-import DataViewAIP from '~/components/DataViewAIP';
 import Statistical from '~/components/Statistical';
 
 const cx = classNames.bind(styles);
