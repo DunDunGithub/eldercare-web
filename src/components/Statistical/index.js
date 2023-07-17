6fa2ab21a3e6e2d10095c028b7e38f2ef293942a
Dun <dun@MYD92s-MacBook-Pro.local>
2023-07-17 12:15:10 +0700

    Dashboard Statistical

--- src/components/Statistical/index.js
+++ src/components/Statistical/index.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
-import styles from './Statistical.module.scss'
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import styles from './Statistical.module.scss';
+import {
+    CartesianGrid,
+    Legend,
+    Line,
+    LineChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from 'recharts';
+
+import { DatePicker } from 'antd';
+import moment from 'moment';
+
+const { RangePicker } = DatePicker;
 
 const cx = classNames.bind(styles);
 
 const data = [
-    {name: "2017", Finished: 1, Incomplete:12, AIP: 5},
-    {name: "2018", Finished: 5, Incomplete:5, AIP: 14},
-    {name: "2019", Finished: 8, Incomplete:4, AIP: 10},
-    {name: "2020", Finished: 3, Incomplete:15, AIP: 8},
-    {name: "2021", Finished: 15, Incomplete:6, AIP: 3},
-    {name: "2022", Finished: 10, Incomplete:18, AIP: 1},
-]
-
-
+    { name: '2017', Finished: 1, Incomplete: 12, AIP: 5 },
+    { name: '2018', Finished: 5, Incomplete: 5, AIP: 14 },
+    { name: '2019', Finished: 8, Incomplete: 4, AIP: 10 },
+    { name: '2020', Finished: 3, Incomplete: 15, AIP: 8 },
+    { name: '2021', Finished: 15, Incomplete: 6, AIP: 3 },
+    { name: '2022', Finished: 10, Incomplete: 18, AIP: 1 },
+];
 
 function Statistical() {
-    return ( 
+    const [dates, setDates] = useState([]);
+
+    console.log(dates);
+
+    return (
         <div className={cx('data-view')}>
             <h2>Thống kê</h2>
+            <div style={{margin:'10px'}}>
+                <RangePicker
+                    onChange={(values) => {
+                        setDates(
+                            values.map((item) => {
+                                return item.format('YYYY-DD-MM');
+                            }),
+                        );
+                    }}
+                />
+            </div>
             <div className={cx('wrap')}>
                 <div className={cx('item-group')}>
                     <div>Công việc đã hoàn thành</div>
@@ -37,28 +64,34 @@ function Statistical() {
             <div className={cx('wrap-chart')}>
                 <ResponsiveContainer width="95%" height={500}>
                     <LineChart data={data}>
-                        <Line type={'monotone'}
-                              dataKey="Finished"
-                              stroke='#2196F3'
-                              strokeWidth={3}/>
-                        <Line type={'monotone'}
-                              dataKey="Incomplete"
-                              stroke='#F44236'
-                              strokeWidth={3}/>
-                        <Line type={'monotone'}
-                              dataKey="AIP"
-                              stroke='#FFCA29'
-                              strokeWidth={3}/>
-                        <CartesianGrid stroke='#ccc'/>
-                        <XAxis dataKey="name"/>
-                        <YAxis/>
-                        <Tooltip/>
-                        <Legend/>
+                        <Line
+                            type={'monotone'}
+                            dataKey="Finished"
+                            stroke="#2196F3"
+                            strokeWidth={3}
+                        />
+                        <Line
+                            type={'monotone'}
+                            dataKey="Incomplete"
+                            stroke="#F44236"
+                            strokeWidth={3}
+                        />
+                        <Line
+                            type={'monotone'}
+                            dataKey="AIP"
+                            stroke="#FFCA29"
+                            strokeWidth={3}
+                        />
+                        <CartesianGrid stroke="#ccc" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
                     </LineChart>
                 </ResponsiveContainer>
             </div>
         </div>
-     );
+    );
 }
 
 export default Statistical;
